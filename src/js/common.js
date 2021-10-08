7dfaf2d54bc95f7c01d58231877407c64a9735b2
NilsRondhuis <nilsrondhuis@ukr.net>
2021-10-08 20:16:01 +0300

    portrait done

--- src/js/common.js
+++ src/js/common.js
@@ -1,10 +1,12 @@
 const refs = {
     listProject: document.querySelector('.list-project'),
     listService: document.querySelector('.list-service'),
+    listNews: document.querySelector('.list-news'),
 }
 
 refs.listProject.addEventListener('click', noReset);
 refs.listService.addEventListener('click', noReset);
+refs.listNews.addEventListener('click', noReset);
 
 function noReset(e) {
     e.preventDefault();
