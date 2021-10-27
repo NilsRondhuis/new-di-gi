08816855888a9fa1c23ab40bdfbb19640bae157b
NilsRondhuis <nilsrondhuis@ukr.net>
2021-10-27 21:11:21 +0300

    done slider-hero

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
+import '../node_modules/swiper/swiper-bundle.min.css';
+import './sass/main.scss';
 import './js/common/common';
 import './js/refs/refs';
 import './js/menu';
 import './js/swiper-hero';
-import '../node_modules/swiper/swiper-bundle'
-import '../node_modules/swiper/swiper-bundle.min.css'
-import './sass/main.scss';
 
--- src/js/swiper-hero.js
+++ src/js/swiper-hero.js
@@ -1,11 +1,19 @@
-import Swiper from 'swiper';
+import Swiper, { Pagination, Mousewheel, Autoplay } from 'swiper';
+
+Swiper.use([Pagination, Mousewheel, Autoplay]);
 
 new Swiper('.swiper', {
     pagination: {
         el: '.swiper-pagination',
         type: 'bullets',
+        clickable: true,
     },
     simulateTouch: true,
     grabCursor: true,
     loop: true,
+    autoplay: {
+        delay: 4000,
+        disableOnInteraction: true,
+    },
+    speed: 2000,
 });
